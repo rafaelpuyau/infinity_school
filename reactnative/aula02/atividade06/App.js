cac5376c7bd36efb89fa685a67289fb253977708
Rafael Puyau <puyau@protonmail.com>
2022-04-03 03:58:20 -0300

    Atualizado com o componente <Pressable />, botão personalizável

--- reactnative/aula02/atividade06/App.js
+++ reactnative/aula02/atividade06/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Pressable } from 'react-native';
 import { useState } from 'react';
 
 export default function App() {
@@ -7,8 +7,12 @@ export default function App() {
   const [name, setName] = useState("");
   const [number, setNumber] = useState(0);
 
-  function sum(){
+  function sum() {
     setNumber(number + 1);
+  }
+
+  function reset() {
+    setNumber(0)
   }
 
   return (
@@ -21,11 +25,23 @@ export default function App() {
         onSubmitEditing={() => name.length < 3 ? alert('Epa') : alert('Ae')}
       /> */}
 
-      <Text>{number}</Text>
-      <Button
+      <Text style={styles.result}>{number}</Text>
+      {/* <Button
         title='Add 1'
         onPress={sum}
-      />
+      /> */}
+
+      <Pressable style={styles.button} onPress={sum}>
+        <Text style={styles.text}>
+          Super ADD Plus
+        </Text>
+      </Pressable>
+
+      <Pressable style={styles.reset} onPress={reset}>
+        <Text style={styles.text}>
+          Reset
+        </Text>
+      </Pressable>
     </View>
   );
 }
@@ -37,4 +53,36 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 12,
+      paddingHorizontal: 32,
+      marginTop: 16,
+      borderRadius: 8,
+      elevation: 3,
+      backgroundColor: '#00AA55',
+      width: 200,
+  },
+  reset: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    marginTop: 16,
+    borderRadius: 8,
+    elevation: 3,
+    backgroundColor: '#FFAA55',
+    width: 200,
+},
+  text: {
+      fontSize: 16,
+      lineHeight: 21,
+      fontWeight: 'bold',
+      letterSpacing: 0.25,
+      color: '#FFFFFF',
+  },
+  result: {
+    fontSize: 72,
+  }
 });
