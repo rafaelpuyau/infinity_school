d0b2e683f3515f8e7b1d5a0765201653b24e913c
Rafael Puyau <puyau@protonmail.com>
2023-02-15 23:38:09 -0300

    Atualização de arquivos

--- logica_programacao/gabarito/parte_1/exerc07.py
+++
@@ -1,9 +0,0 @@
-'''
-Faça um programa que imprima 20 números (de 0 à 19) em ordem decrescente e um do lado do outro.
-'''
-
-# Para imprimir na ordem decrescente, a função Range
-# teve o start com 19, o end com -1, pois queremos imprimir
-# o 0 e o passo foi de -1 para que haja o decréscimo dos números
-for numero in range(19, -1, -1):
-    print(numero, end=' ')
--- logica_programacao/gabarito/parte_2/exerc01.py
+++
@@ -1,6 +0,0 @@
-'''
-Escreva um programa que mostre na tela a mensagem "Olá, mundo!".
-'''
-
-# Usamos a função print para exibir uma mensagem na tela
-print('Olá, mundo!')
